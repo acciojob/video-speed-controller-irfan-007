c8dc6d7a1a6e28afcc8737b9d1f8d9bef250f230
irfan <85334710+irfan-007@users.noreply.github.com>
2024-07-31 16:17:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,35 @@
-const inputs = document.querySelectorAll('.controls input');
+let video = document.querySelector('.player__video');
+let speed=document.querySelector('.speed-bar');
+let backward=document.querySelector('.rewind');
+let forward=document.querySelector('.forward');
 
-    function handleUpdate() {
-      const suffix = this.dataset.sizing || '';
-      document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
-    }
+forward.addEventListener("click",forwardFn);
+backward.addEventListener("click",backwardFn);
 
-    inputs.forEach(input => input.addEventListener('change', handleUpdate));
-    inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+
+speed.addEventListener("click",(e)=>{
+	if(speed.innerHTML=="1x"){
+		speed.innerHTML="2x";
+		video.playbackRate=2;
+	}
+	else if(speed.innerHTML=="2x"){
+		speed.innerHTML="4x";
+		video.playbackRate=4;
+	}
+	else if(speed.innerHTML=="4x"){
+		speed.innerHTML="0.5x";
+		video.playbackRate=0.5;
+	}
+	else {
+		speed.innerHTML="1x";
+		video.playbackRate=1;
+	}
+})
+function backwardFn(e){
+	video.currentTime=video.currentTime-10;
+}    
+function forwardFn(e){
+	video.currentTime=video.currentTime+25;
+}
+
+
